3dbd28fcb739f1802d86f2d8b11472fe87ec9bfd
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-10-19 08:48:25 -0500

    step 2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookModule } from './book/book.module';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BookModule,
     BrowserModule,
     AppRoutingModule
   ],
